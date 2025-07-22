b7af782c72de676d1205a54c89266fbc245705ea
Avadhut Noola <avdhutnula@gmail.com>
2025-07-22 14:43:46 +0530

    Ensures all shared interfaces are available

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // src/index.ts
 // src/index.ts - Main export file
-export { CognitoService, CognitoConfigs } from './services/cognito';
-export { CognitoConfig, PackageConfig } from './config/packageConfig';
+export { CognitoService, CognitoConfigs } from './services/cognito.js';
+export { CognitoConfig, PackageConfig } from './config/packageConfig.js';
 
 // Ensures all shared interfaces are available
-export * from "./types";
+export * from './types/index.js';
 
-// Exporting all functions for easier access
-export { extractTokens } from "./utils/tokenManager";
+// Exporting functions for easier access
+export { extractTokens } from "./utils/tokenManager.js";
 
 
